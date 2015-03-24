65e28af24cfe25684ff35b4e49d35f4eb9c17cfe
Kanin Sirisith <arm.se.th@gmail.com>
2015-03-24 23:11:16 +0700

    implement filter function

--- public/assets/ng/internship.js
+++ public/assets/ng/internship.js
@@ -1,6 +1,7 @@
 app.controller('InternshipThaiCtrl', [
   '$scope','restService',
   function($scope, restService) {
+    $scope.isThai = true
     $scope.filter = []
     restService.getPlaceThailand().then(function(res) {
       var places = res.data
@@ -48,25 +49,77 @@ app.controller('InternshipThaiCtrl', [
 app.controller('InternshipOverseasCtrl', [
   '$scope','restService',
   function($scope, restService) {
+    $scope.isThai = false
+    $scope.filter = []
 
     restService.getPlaceOverseas().then(function(res) {
       var places = res.data
       restService.getTag().then(function(resp) {
         var tags = resp.data
+        for(var i = 0; i < tags.length; i++) {
+          for(var j = 0; j < tags[i].tags.length; j++) {
+            $scope.filter.push(tags[i].tags[j].id)
+          }
+        }
         restService.getAvailableCountry().then(function(countries){
             var ava_countries = countries.data
             var thai = -1;
             for(var i = 0 ; i < ava_countries.length ; ++i) {
-              if(ava_countries[i].name == "Thailand")
+              if(ava_countries[i].name == "Thailand") {
                 thai = i;
+              } else {
+                $scope.filter.push(ava_countries[i].name)
+              }
             }
             if(thai != -1){
               ava_countries.splice(thai, 1);
             }
 
+
             $scope.places_in_thailand = {'places' : places , 'filters': tags , 'ava_countries' : ava_countries}
         })
       })
-    })   
+    })
+
+    $scope.filterplace = function(place) {
+      // console.log('filtering')
+      // console.log(place)
+      for(var i = 0; i < place.tags.length; i++) {
+        if($scope.filter.indexOf(place.tags[i].tag_id) == -1) {
+          return false
+        }
+      }
+      if($scope.filter.indexOf(place.country_name) == -1) {
+        return false
+      }
+      return true
+    }
+
+    $scope.filtercheck = function(val, option) {
+      var index = $scope.filter.indexOf(option.id)
+      if(val) {
+        if(index == -1) {
+          $scope.filter.push(option.id)
+        }
+      } else {
+        if(index != -1) {
+          $scope.filter.splice(index,1)
+        }
+      }
+    }
+
+    $scope.filtercountry = function(val, option) {
+      var index = $scope.filter.indexOf(option.name)
+      if(val) {
+        if(index == -1) {
+          $scope.filter.push(option.name)
+        }
+      } else {
+        if(index != -1) {
+          $scope.filter.splice(index,1)
+        }
+      }
+    }
+
   }
 ])
